433b3c29dd65960eb911326efc9487eb3b8ca69c
Beaver <liviubalan31@gmail.com>
2024-01-05 20:36:33 +0200

    v0.33 cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -18,5 +18,26 @@ int main() {
     cout<<usb2;
     OpenWindow();
 
+
+
+
+    resistor res1(120, "0805");
+    res1.rescalc(usb2, 2);
+    caps cap1(20, "0450");
+    cap1.cap_select();
+    //Pointers to PowersSum function
+    diode diode1("0805");
+    cout<<diode1;
+    LED led1("0805");
+    led1.set_led();
+    cout<<led1;
+    diode* diode1Pointer;
+    diode1Pointer = &diode1;
+    LED* led1Pointer;
+    led1Pointer = &led1;
+
+    cout<<"\n"<<diode1Pointer->PowerSum();
+    cout<<"\n"<<led1Pointer->PowerSum();
     return 0;
+
 }
